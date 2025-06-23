ecce55fa2b0f9f87fb3bb23e6343b96c05da7a20
partisani <88511157+partisani@users.noreply.github.com>
2025-06-23 14:06:36 -0300

    refactor+fix(asset): new themes and wallpapers, base24, and organization
    assets/walls now includes subdirectories, and task.lua was modified
    to properly recognize these and as a side-effect now converts wallpapers
    in parallel (using nushell's par-each)
    
    the themes eink and eink-faulty were added and base24 support for the
    base16 theme importer can be enabled by calling it with a non-nil second
    argument (as a convention, use the string "base24")

--- assets/themes/base16.lua
+++ assets/themes/base16.lua
@@ -10,8 +10,9 @@ function os.capture(cmd, raw)
     return s
 end
 
-return function(scheme)
-    local command = "yq -o=lua '.' assets/themes/base16-schemes/base16/" .. scheme .. ".yaml"
+return function(scheme, use24)
+    local command = "yq -o=lua eval '... comments=\"\"' assets/themes/base16-schemes/base" ..
+        (use24 and "24/" or "16/") .. scheme .. ".yaml"
     scheme = load(os.capture(command))()
 
     local palette = scheme.palette
@@ -21,6 +22,8 @@ return function(scheme)
         [0] = palette.base00, palette.base01, palette.base02, palette.base03,
               palette.base04, palette.base05, palette.base06, palette.base07,
               palette.base08, palette.base09, palette.base0A, palette.base0B,
-              palette.base0C, palette.base0D, palette.base0E, palette.base0F
+              palette.base0C, palette.base0D, palette.base0E, palette.base0F,
+              palette.base11, palette.base12, palette.base13, palette.base14, -- basically nil
+              palette.base15, palette.base16, palette.base17                  -- if non existent
     }
 end
